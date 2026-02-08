1cf0c1cc2894764154a31171ddc4236cc3a0258d
Robin Dorst <robin@dorsti.de>
2026-02-08 17:16:19 +0100

    set correct site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://lopfi.github.io',
+	base: '/cnc-wiki',
 	integrations: [
 		starlight({
 			title: 'My Docs',
